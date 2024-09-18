a206afe0388228d0fa4dc6afba7e8f9bb60ceb92
orailean <orailean@amsoft-group.com>
2024-09-18 13:41:22 +0300

    Initial commit

---
+++ person-service/src/test/java/com/rayn_microservices/person_service/PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayn_microservices.person_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
